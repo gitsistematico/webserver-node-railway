59c901141cb7923372572f9c83651e9c316c3935
RicardoRincon <ricardo.rincon@medismart.live>
2023-09-08 18:16:20 -0500

    first commit

--- app.js
+++ app.js
@@ -9,48 +9,51 @@ const port = process.env.PORT;
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials', function (err) {});
 
-// app.get('/', (req, res) => {
-//     res.render('home',{
-//         nombre: 'Ricardo Rincon',
-//         titulo: 'Curso de Node'
-//     });
-// })
-
-// app.get('/generic', (req, res) => {
-//     res.render('generic',{
-//         nombre: 'Ricardo Rincon',
-//         titulo: 'Curso de Node'
-//     });
-// })
-
-// app.get('/elements', (req, res) => {
-//     res.render('elements',{
-//         nombre: 'Ricardo Rincon',
-//         titulo: 'Curso de Node'
-//     });
-// })
-// servir contenido estativo
 app.use( express.static('public'));
+
+app.get('/home', (req, res) => {
+    res.render('home',{
+        nombre: 'Ricardo Rincon',
+        titulo: 'Curso de Node'
+    });
+})
+
+app.get('/generic', (req, res) => {
+    res.render('generic',{
+        nombre: 'Ricardo Rincon',
+        titulo: 'Curso de Node'
+    });
+})
+
+app.get('/elements', (req, res) => {
+    res.render('elements',{
+        nombre: 'Ricardo Rincon',
+        titulo: 'Curso de Node'
+    });
+})
+
+// ************************ servir contenido estativo ******************************
+// app.use( express.static('public'));
 
 // app.get('/hola-mundo', (req, res) => {
 //     res.send('Hola mundo')
 // })
 
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/public/index.html');
-// })
+// // app.get('/', (req, res) => {
+// //     res.sendFile(__dirname + '/public/index.html');
+// // })
 
 // app.get('/generic', (req, res) => {
-//     res.sendFile(__dirname + '/public/generic.html');
+//     res.sendFile(__dirname + '/public/template/generic.html');
 // })
 
 // app.get('/elements', (req, res) => {
-//     res.sendFile(__dirname + '/public/elements.html');
+//     res.sendFile(__dirname + '/public/template/elements.html');
 // })
 
-app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-})
+// app.get('*', (req, res) => {
+//     res.sendFile(__dirname + '/public/404.html');
+// })
 app.listen(port, () => {
     console.log(`Listening at port: ${ port }`);
 })
